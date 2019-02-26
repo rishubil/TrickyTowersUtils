707feadfbfee4241688c8b3bcf37d6b62272881b
Nesswit <rishubil@gmail.com>
2019-02-26 22:54:02 +0900

    Add chardet to open config file

--- config.py
+++ config.py
@@ -1,11 +1,26 @@
 import configparser
+from chardet.universaldetector import UniversalDetector
+
+CONFIG_FILE = 'config.ini'
+
+
+def detect_encoding():
+    with open(CONFIG_FILE, 'rb') as f:
+        detector = UniversalDetector()
+        detector.reset()
+        for line in f.readlines():
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result['encoding']
 
 
 class Config:
     def __init__(self):
         try:
             self._config = configparser.ConfigParser()
-            self._config.read('config.ini', encoding='utf-8')
+            self._config.read(CONFIG_FILE, encoding=detect_encoding())
         except Exception as e:
             raise RuntimeError('Cannot open configuration file') from e
 
